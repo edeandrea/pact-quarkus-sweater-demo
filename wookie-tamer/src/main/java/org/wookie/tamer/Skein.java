3c3d46b4d30df2d7780bf27292fe6e9afc48349b
Eric Deandrea <eric.deandrea@gmail.com>
2023-03-17 16:35:46 -0400

    Random staple length & survival of the tamer

--- wookie-tamer/src/main/java/org/wookie/tamer/Skein.java
+++ wookie-tamer/src/main/java/org/wookie/tamer/Skein.java
@@ -1,7 +1,5 @@
 package org.wookie.tamer;
 
-import java.util.Random;
-
 public record Skein(String colour, int orderNumber, StapleLength stapleLength, boolean tamerSurvived) {
 
     /**
@@ -10,6 +8,6 @@ public record Skein(String colour, int orderNumber, StapleLength stapleLength, b
      * @param wookieColor the wookieColor whose fur will be spun into a skein of yarn
      */
     public Skein(WookieColor wookieColor, int orderNumber, boolean tamerSurvived) {
-			this(wookieColor.name().toLowerCase(), orderNumber, StapleLength.values()[new Random().nextInt(0, StapleLength.values().length)], tamerSurvived);
+			this(wookieColor.name().toLowerCase(), orderNumber, StapleLength.findRandom(), tamerSurvived);
     }
 }
--- wookie-tamer/src/main/java/org/wookie/tamer/StapleLength.java
+++ wookie-tamer/src/main/java/org/wookie/tamer/StapleLength.java
@@ -1,5 +1,11 @@
 package org.wookie.tamer;
 
+import java.util.Random;
+
 public enum StapleLength {
-	Short, Medium, Long
+	Short, Medium, Long;
+
+	public static StapleLength findRandom() {
+		return values()[new Random().nextInt(0, values().length)];
+	}
 }
