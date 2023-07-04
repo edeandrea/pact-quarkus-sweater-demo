f19e49cbc2f2841acf21b5d112b9e02db7eb2529
Eric Deandrea <eric.deandrea@gmail.com>
2023-07-04 08:06:54 +0200

    Change to use a paramerterized test

--- wookie-tamer/src/test/java/org/wookie/tamer/FurResourceTest.java
+++ wookie-tamer/src/test/java/org/wookie/tamer/FurResourceTest.java
@@ -3,7 +3,8 @@ package org.wookie.tamer;
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import io.quarkus.test.junit.QuarkusTest;
 
@@ -12,10 +13,9 @@ import io.restassured.http.ContentType;
 @QuarkusTest
 public class FurResourceTest {
 
-    // We should make these parameterised tests
-    @Test
-    public void furEndpointForWhiteFur() {
-        String colour = "white";
+    @ParameterizedTest
+    @ValueSource(strings = { "white", "black" })
+    public void furEndpoint(String colour) {
         Order order = new Order(colour, 2);
         Skein skein = given()
                 .contentType(ContentType.JSON)
@@ -28,21 +28,4 @@ public class FurResourceTest {
 
         assertEquals(skein.colour(), colour);
     }
-
-    @Test
-    public void furEndpointForBlackFur() {
-        String colour = "black";
-        Order order = new Order(colour, 4);
-        Skein skein = given()
-                .contentType(ContentType.JSON)
-                .body(order)
-                .when()
-                .post("/fur/order")
-                .then()
-                .statusCode(200)
-                .extract().as(Skein.class);
-
-        assertEquals(skein.colour(), colour);
-    }
-
 }
